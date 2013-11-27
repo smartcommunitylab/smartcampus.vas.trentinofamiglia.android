1bbc247141b681864920572a2ad78c9dc93d7697
chin8 <m.chini@trentorise.eu>
2013-11-27 15:28:59 +0100

    New icons and some colors

--- src/eu/trentorise/smartcampus/trentinofamiglia/custom/CategoryHelper.java
+++ src/eu/trentorise/smartcampus/trentinofamiglia/custom/CategoryHelper.java
@@ -63,21 +63,21 @@ public class CategoryHelper {
 	public static final String CATEGORY_MY = "My";
 
 	public static CategoryDescriptor[] EVENT_CATEGORIES = new CategoryDescriptor[] {
-			/* 1 */new CategoryDescriptor(R.drawable.ic_marker_e_family, R.drawable.ic_summer,
+			/* 1 */new CategoryDescriptor(R.drawable.ic_estate_map, R.drawable.ic_summer,
 					CAT_EVENT_ESTATE_GIOVANI_E_FAMIGLIA, R.string.categories_event_summer_family),
-			/* 2 */new CategoryDescriptor(R.drawable.ic_marker_e_family, R.drawable.ic_altogarda, 
+			/* 2 */new CategoryDescriptor(R.drawable.ic_altogarda_map, R.drawable.ic_altogarda, 
 					CAT_EVENT_ALTO_GARDA, R.string.categories_event_alto_garda), };
 
 	public static CategoryDescriptor[] POI_CATEGORIES = new CategoryDescriptor[] {
-			/* 1 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.family_audit, CAT_POI_FAMILY_AUDIT,
+			/* 1 */new CategoryDescriptor(R.drawable.ic_familyaudit_map, R.drawable.family_audit, CAT_POI_FAMILY_AUDIT,
 					R.string.categories_poi_family_audit),
-			/* 2 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_beach, CAT_POI_VACANZE_AL_MARE,
+			/* 2 */new CategoryDescriptor(R.drawable.ic_mare_map, R.drawable.ic_beach, CAT_POI_VACANZE_AL_MARE,
 					R.string.categories_poi_summer_holyday),
-			/* 3 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_media,
+			/* 3 */new CategoryDescriptor(R.drawable.ic_poi, R.drawable.ic_media,
 					CAT_POI_TAVOLO_NUOVI_MEDIA, R.string.categories_poi_new_media),
-			/* 4 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_breastfeeding, CAT_POI_PUNTI_ALLATTAMENTO,
+			/* 4 */new CategoryDescriptor(R.drawable.ic_allattamento_map, R.drawable.ic_breastfeeding, CAT_POI_PUNTI_ALLATTAMENTO,
 					R.string.categories_poi_breast_point),
-			/* 5 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.family_trentino,
+			/* 5 */new CategoryDescriptor(R.drawable.ic_family_tn_map, R.drawable.family_trentino,
 					CAT_POI_FAMILY_IN_TRENTINO, R.string.categories_poi_trentino_family), }; 
 
 	public static CategoryDescriptor[] INFO_CATEGORIES = new CategoryDescriptor[] {
@@ -95,11 +95,11 @@ public class CategoryHelper {
 					CAT_INFO_DISTRETTI_E_ORGANIZZAZIONI, R.string.categories_info_organizations), };
 
 	public static CategoryDescriptor[] TRACK_CATEGORIES = new CategoryDescriptor[] {
-			/* 1 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_bici, CAT_TRACK_PISTE_CICLOPEDONALI,
+			/* 1 */new CategoryDescriptor(R.drawable.ic_ciclopedonali_map, R.drawable.ic_bici, CAT_TRACK_PISTE_CICLOPEDONALI,
 					R.string.categories_track_pedestrian),
-			/* 2 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_passeggiata, CAT_TRACK_PASSEGGIATE,
+			/* 2 */new CategoryDescriptor(R.drawable.ic_bici, R.drawable.ic_passeggiata, CAT_TRACK_PASSEGGIATE,
 					R.string.categories_track_trekking),
-			/* 3 */new CategoryDescriptor(R.drawable.ic_marker_p_family, R.drawable.ic_bici,
+			/* 3 */new CategoryDescriptor(R.drawable.ic_bici, R.drawable.ic_bici,
 					CAT_TRACK_PISTA_CICLABILE, R.string.categories_track_bicycle), };
 
 	private static Map<String, String> categoryMapping = new HashMap<String, String>();
